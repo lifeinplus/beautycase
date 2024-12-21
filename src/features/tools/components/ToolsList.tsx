d0d1a7ae5aa21f03067a355aa2369ad600706343
Artem Denisov <lifeinplus@gmail.com>
2024-12-21 20:48:40 +0100

    BC-80 Enhance ToolsList and ToolDetailsPage for improved navigation and scrolling behavior

--- src/features/tools/components/ToolsList.tsx
+++ src/features/tools/components/ToolsList.tsx
@@ -1,3 +1,6 @@
+import { useCallback, useEffect, useState } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
+
 import { Tool } from '../types'
 
 interface ToolsListProps {
@@ -5,17 +8,47 @@ interface ToolsListProps {
 }
 
 export const ToolsList = ({ tools }: ToolsListProps) => {
+    const { pathname, state } = useLocation()
+    const navigate = useNavigate()
+
+    const [scrolled, isScrolled] = useState(false)
+
+    const scroll = useCallback((node: HTMLLIElement | null) => {
+        if (!node) return
+
+        const { top, height } = node.getBoundingClientRect()
+
+        window.scrollTo({
+            top: top - height / 2,
+            behavior: 'instant',
+        })
+
+        isScrolled(true)
+    }, [])
+
+    useEffect(() => {
+        navigate(pathname, { replace: true })
+    }, [scrolled])
+
+    const handleClick = (id?: string) => {
+        navigate(`/tools/${id}`, {
+            state: { fromPathname: pathname, fromMakeupBag: true },
+        })
+    }
+
     return (
         <ul className="mx-auto mt-6 flex list-none flex-col items-center gap-8 sm:flex-row">
             {tools.map((tool, index) => (
                 <li
                     key={index}
-                    className="flex flex-col items-center p-2 sm:w-5/6"
+                    className="flex w-full flex-col items-center p-2"
+                    onClick={() => handleClick(tool._id)}
+                    ref={tool._id === state?.scrollId ? scroll : null}
                 >
                     <img
-                        src={tool.image}
                         alt={tool.name}
                         className="mb-6 w-1/2 rounded"
+                        src={tool.image}
                     />
                     <h6 className="text-center font-heading text-sm">
                         {tool.name}
